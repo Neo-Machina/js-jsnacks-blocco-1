ba56faed8c4d54ba54fc0b41b143a20b5b01e3bb
Neo-Machina <andreistatache1997@gmail.com>
2022-06-09 17:01:15 +0200

    jsnak3 completed

---
+++ jsnack3/js/script.js
@@ -0,0 +1,16 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// Il software chiede per 10 volte all’utente di inserire un numero
+let sumNumbers = 0;
+for( i = 0; i < 10; i++) {
+    const number = parseInt( prompt('Inserisci un numero') ); 
+    
+    sumNumbers += number;
+    console.log(sumNumbers);
+}
+
+// Stampo la somma di tutti i numeri inseriti
+document.getElementById('sum-numbers').innerHTML = `La somma totale è ${sumNumbers}`;
+
+
